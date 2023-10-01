078ae4c34d88b04a8a3b4c46e928bf828dc892b5
Kiran Yaramala <kiran.yaramala@infor.com>
2023-10-01 14:14:14 +0530

    add partials template

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const path=require("path")
 const express=require("express")
+const hbs=require("hbs")
 
 
-const publicDir=path.join(__dirname,"../public");
 const app=express()
 
+// #Define path for express config
+const publicDir=path.join(__dirname,"../public");
+const viewPath=path.join(__dirname,'../templates/views')
+const partialPath=path.join(__dirname,'../templates/partials')
+
+//setup handlebars engine
 app.set('view engine','hbs')
+app.set('views',viewPath)
+hbs.registerPartials(partialPath)
+
 app.use(express.static(publicDir))
 
 app.get('',(req,res)=>{
@@ -16,7 +25,9 @@ app.get('',(req,res)=>{
 })
 app.get('/help',(req,res)=>{
     res.render("help",{
-        msg:"Some data"
+        title:"Weather",
+        msg:"Some data",
+        uname:"Kiran"
     })
 })
 app.get('/about',(req,res)=>{
