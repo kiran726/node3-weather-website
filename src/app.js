8608c7b599d151c81e83267eefcb6c4b7a6c854a
Kiran Yaramala <kiran.yaramala@infor.com>
2023-10-01 11:56:02 +0530

    dynamic pages with templating using hbs

--- src/app.js
+++ src/app.js
@@ -4,8 +4,27 @@ const express=require("express")
 
 const publicDir=path.join(__dirname,"../public");
 const app=express()
+
+app.set('view engine','hbs')
 app.use(express.static(publicDir))
 
+app.get('',(req,res)=>{
+    res.render("index",{
+        title:"Weather",
+        uname:"Kiran"
+    })
+})
+app.get('/help',(req,res)=>{
+    res.render("help",{
+        msg:"Some data"
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render("about",{
+        title:"About Me",
+        uname:"Kiran"
+    })
+})
 app.listen(3000,()=>{
     console.log("Server is up on port 3000")
 })
