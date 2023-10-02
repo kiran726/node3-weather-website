e21e24012ab5f6bcc95219851b8c6177a1bc24ff
Kiran Yaramala <kiran.yaramala@infor.com>
2023-10-02 16:29:15 +0530

    setup app

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const forecast=require("../util/forecast")
 const geocode=require("../util/geocode")
 
 const app=express()
-
+const port=process.env.PORT || 3000
 // #Define path for express config
 const publicDir=path.join(__dirname,"../public");
 const viewPath=path.join(__dirname,'../templates/views')
@@ -80,6 +80,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log("Server is up on port 3000")
+app.listen(port,()=>{
+    console.log("Server is up on port "+port)
 })
